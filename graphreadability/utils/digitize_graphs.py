dd72a569cb1e4186d41f5e27523725d7350f6ea7
Philip Mathieu <philip.eng.mathieu@gmail.com>
2024-03-04 11:53:32 -0500

    Fix digitizer writing

--- graphreadability/utils/digitize_graphs.py
+++ graphreadability/utils/digitize_graphs.py
@@ -4,14 +4,23 @@ import networkx as nx
 # Default image path
 DEFAULT_IMAGE_PATH = "figs/Dunne et al 2015 Figure 1.jpg"
 DEFAULT_FILE_NAME = "graphs/graph.graphml"
+NEXT_NODE = 1
+
+
+# Generate a node label with the next available integer
+def get_next_id():
+    global NEXT_NODE
+    label = "Node " + str(NEXT_NODE)
+    NEXT_NODE += 1
+    return label
 
 
 # Check if the node is near an existing node
-def nearby_nodes(nodes, x, y, radius=10):
-    for _, node in nodes.items():
-        if ((node["pos"][0] - x) ** 2 + (node["pos"][1] - y) ** 2) ** 0.5 < radius:
-            return node
-    return False
+def nearby_nodes(nodes, x, y, radius=15):
+    for label, node in nodes:
+        if ((node["pos_x"] - x) ** 2 + (node["pos_y"] - y) ** 2) ** 0.5 < radius:
+            return label, node
+    return None, None
 
 
 def main(args):
@@ -34,10 +43,28 @@ def main(args):
 
     # Display the image
     fig, ax = plt.subplots()
+    fig.tight_layout()
     ax.imshow(img)
+    ax.set_xticks([])
+
+    # Label with the filename
+    ax.set_title(image_path)
+
+    # Add a caption with instructions
+    ax.text(
+        0.5,
+        0,
+        "Click to add nodes. Click two existing nodes to add an edge.\nRight click to delete a node and its edges. Press 'q' to quit.",
+        transform=ax.transAxes,
+        verticalalignment="top",
+        horizontalalignment="center",
+        fontsize=10,
+        color="black",
+        backgroundcolor=(1, 1, 1, 0.5),
+    )
 
     # Set up the graph
-    nodes = {}
+    nodes = []
     edges = []
 
     # Set up the click event handler
@@ -45,57 +72,115 @@ def main(args):
     EDGE_START = None
 
     def on_click(event):
-        """Handle mouse clicks on the plot."""
+        """Handle single mouse clicks on the plot."""
+        # Check if the click is in the plot
+        if event.inaxes is None:
+            return
         nonlocal IN_EDGE_CREATION, EDGE_START
-        x, y = int(event.xdata), int(event.ydata)
 
-        # Check if the click is on an existing node
-        existing_node = nearby_nodes(nodes, x, y)
+        if event.button == 1:
+            # Left click
 
-        if not IN_EDGE_CREATION:
-            if existing_node:
-                # Start edge creation mode
-                IN_EDGE_CREATION = True
-                EDGE_START = existing_node
-                print(f"Creating edge from {existing_node}")
+            x, y = int(event.xdata), int(event.ydata)
+
+            # Check if the click is on an existing node
+            label, existing_node = nearby_nodes(nodes, x, y)
+
+            if not IN_EDGE_CREATION:
+                if existing_node:
+                    # Start edge creation mode
+                    IN_EDGE_CREATION = True
+                    EDGE_START = (label, existing_node)
+                    print(f"Creating edge from {label}")
+                else:
+                    # Add a new node
+                    label = get_next_id()
+                    nodes.append((label, {"pos_x": x, "pos_y": y}))
+                    print(f"Added node {label} at ({x}, {y})")
+                    # Update the plot
+                    ax.scatter(x, y, color="r")
+                    ax.annotate(label, (x, y))
+                    plt.draw()
             else:
-                # Add a new node
-                nodes[(x, y)] = {"label": "Node " + str(len(nodes) + 1), "pos": (x, y)}
-                print(f"Added node {nodes[(x, y)]['label']} at ({x}, {y})")
-                # Update the plot
-                ax.scatter(x, y, color="r")
-                ax.annotate(nodes[(x, y)]["label"], (x, y))
-                plt.draw()
-        else:
-            # Finish edge creation mode
+                # Finish edge creation mode
+                IN_EDGE_CREATION = False
+                if existing_node:
+                    print(f"Creating edge from {EDGE_START[0]} to {label}")
+                    edges.append((EDGE_START[0], label))
+                    # Update the plot
+                    ax.plot(
+                        [EDGE_START[1]["pos_x"], existing_node["pos_x"]],
+                        [EDGE_START[1]["pos_y"], existing_node["pos_y"]],
+                        color="b",
+                    )
+                    plt.draw()
+                else:
+                    # Add a node and complete the edge
+                    label = get_next_id()
+                    nodes.append((label, {"pos_x": x, "pos_y": y}))
+                    print(f"Added node {label} at ({x}, {y})")
+                    edges.append((EDGE_START[0], label))
+                    # Update the plot
+                    ax.plot(
+                        [EDGE_START[1]["pos_x"], x],
+                        [EDGE_START[1]["pos_y"], y],
+                        color="b",
+                    )
+                    ax.scatter(x, y, color="r")
+                    ax.annotate(label, (x, y))
+                    plt.draw()
+
+        elif event.button == 3:
+            # Right click
+
+            # Negate edge creation mode
             IN_EDGE_CREATION = False
+            EDGE_START = None
+
+            x, y = int(event.xdata), int(event.ydata)
+
+            # Check if the click is on an existing node
+            key, existing_node = nearby_nodes(nodes, x, y)
+
             if existing_node:
-                print(f"Creating edge from {EDGE_START} to {existing_node}")
-                edges.append((EDGE_START, existing_node))
-                # Update the plot
-                ax.plot(
-                    [EDGE_START["pos"][0], existing_node["pos"][0]],
-                    [EDGE_START["pos"][1], existing_node["pos"][1]],
-                    color="b",
-                )
+                # Delete associated edges
+                for edge in list(edges):
+                    if existing_node in edge:
+                        edges.remove(edge)
+                        print(
+                            f"Deleting edge from {edge[0]['label']} to {edge[1]['label']}"
+                        )
+
+                # Delete the node and re-draw the plot
+                print(f"Deleting node {existing_node['label']}")
+                del nodes[key]
+                ax.cla()
+                ax.imshow(img)
+                for _, node in nodes.items():
+                    ax.scatter(node["pos_x"], node["pos_y"], color="r")
+                    ax.annotate(node["label"], (node["pos_x"], node["pos_y"]))
+                for edge in edges:
+                    ax.plot(
+                        [edge[0]["pos_x"], edge[1]["pos_x"]],
+                        [edge[0]["pos_y"], edge[1]["pos_y"]],
+                        color="b",
+                    )
                 plt.draw()
-            else:
-                # Add a node and complete the edge
-                nodes[(x, y)] = {"label": "Node " + str(len(nodes) + 1), "pos": (x, y)}
-                print(f"Added node {nodes[(x, y)]['label']} at ({x}, {y})")
-                edges.append((EDGE_START, nodes[(x, y)]))
-                # Update the plot
-                ax.plot(
-                    [EDGE_START["pos"][0], x],
-                    [EDGE_START["pos"][1], y],
-                    color="b",
-                )
-                ax.scatter(x, y, color="r")
-                ax.annotate(nodes[(x, y)]["label"], (x, y))
-                plt.draw()
+
+    def on_key(event):
+        """Handle single key presses on the plot."""
+        # Close on q
+        if event.key == "q":
+            plt.close()
+        # Cancel edge creation on escape
+        elif event.key == "escape":
+            nonlocal IN_EDGE_CREATION, EDGE_START
+            IN_EDGE_CREATION = False
+            EDGE_START = None
 
     # Connect the click event handler
     cid = fig.canvas.mpl_connect("button_press_event", on_click)
+    qid = fig.canvas.mpl_connect("key_press_event", on_key)
 
     # Display the plot and wait for clicks
     plt.show()
@@ -103,11 +188,31 @@ def main(args):
     # Disconnect event handler when plot is closed
     plt.disconnect(cid)
 
-    # Print the created graph structure
+    # Create a graph from the nodes and edges
     G = nx.Graph()
+    # Convert nodes to list of tuples
     G.add_nodes_from(nodes)
-    print(G.nodes())
-    print(G.edges())
+    # Convert edges to list of tuples
+    G.add_edges_from(edges)
+
+    # Display the graph with networkx
+    plt.clf()
+    x = nx.get_node_attributes(G, "pos_x")
+    y = nx.get_node_attributes(G, "pos_y")
+    pos = {k: (x[k], y[k]) for k in x}
+    nx.draw(
+        G,
+        pos=pos,
+        node_size=100,
+        with_labels=True,
+    )
+    # Reverse the y-axis to match the image
+    plt.gca().invert_yaxis()
+    ax.set_title("Captured Graph")
+    plt.show()
+
+    # Disconnect event handler when plot is closed
+    plt.disconnect(qid)
 
     # Prompt to save the graph
     save_graph = input("Save the graph? (y/[n]): ")
