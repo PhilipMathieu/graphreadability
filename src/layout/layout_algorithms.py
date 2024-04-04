769cfb372fa6b8f06575b2e07882ddfadde1f99b
Philip Mathieu <philip.eng.mathieu@gmail.com>
2024-04-04 11:14:30 -0400

    Add default optimize function

--- src/layout/layout_algorithms.py
+++ src/layout/layout_algorithms.py
@@ -35,7 +35,7 @@ def naive_optimizer(M_input: MetricsSuite, inplace=False):
     poss = {}
     for layout_name, layout_func in layouts.items():
         M = M_input.copy() if not inplace else M_input
-        poss[layout_name] = layout_func(M._graph)
+        poss[layout_name] = layout_func(M.G)
         M.apply_layout(poss[layout_name])
         M.calculate_metrics()
         scores[layout_name] = M.combine_metrics()
@@ -47,3 +47,8 @@ def naive_optimizer(M_input: MetricsSuite, inplace=False):
 
     print(f"Best layout: {best_layout}. Score: {scores[best_layout]}")
     return M
+
+
+def optimize(M_input: MetricsSuite, inplace=False):
+    """Default optimizer for the graph layout."""
+    return naive_optimizer(M_input, inplace=inplace)
